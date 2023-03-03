cf3606c0e7e22955c41fe3d01f495fdda20620b2
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-03-03 08:18:10 +0800

    Add previous graded programming exercises

---
+++ Discovery Series #08 RISC-V integer programming/sym1.asm
@@ -0,0 +1,101 @@
+# Macro to exit program. 
+.macro DONE
+  li a7, 10
+  ecall
+.end_macro 
+
+.macro NEWLINE
+  li a7, 11
+  li a0, 10
+  ecall
+.end_macro
+
+.data
+  prompt_dna: .asciz "Please input DNA string here: "
+  dna_string: .space 101  # 1 ASCII char = 1 byte (8 bits)
+  rna_string: .space 101
+  msg_dna_length: .asciz "Length of DNA string: "
+  msg_rna_converted: .asciz "Converted RNA string: "
+
+.text
+start:
+  # -- INPUT --
+  # Prompt
+  li a7, 4
+  la a0, prompt_dna
+  ecall
+  # Read string
+  li a7, 8
+  la a0, dna_string
+  li a1, 101
+  ecall
+  
+  # -- PROCESS --
+  la t1, dna_string
+  
+  # Find length of string
+  li t2, 0  # init length counter
+loop:
+  lb t3, (t1)  # load byte at beginning of string
+  # if end of string, exit loop
+  beq t3, zero, continue  
+  # else, continue
+  # increment
+  addi t2, t2, 1  # increment the counter
+  addi t1, t1, 1  # increment the address
+  j loop
+  
+continue:
+  # Replace all occurences of T in DNA string to U to form an RNA string
+  la t1, dna_string
+  la t4, rna_string
+  
+loop2:
+  lb t3, (t1) # load byte at beginning of string
+  # if end of string, exit loop
+  beq t3, zero, end  
+  # else, continue
+  # if byte is is 'T', replace with 'U'
+  li t5, 'T'
+  beq t3, t5, replace_T_with_U
+  # else, copy as-is
+  sb t3, (t4)
+  # increment
+  j increment
+  
+replace_T_with_U:
+  li t3, 'U'
+  sb t3, (t4)
+  
+increment:
+  addi t1, t1, 1  # increment the address of DNA string
+  addi t4, t4, 1  # increment the address of RNA string
+  j loop2
+  
+end:  
+  # -- OUTPUT --
+  # Print length of string
+  li a7, 4
+  la a0, msg_dna_length
+  ecall
+  
+  li a7, 1
+  addi t2, t2, -1  # Assuming length of string should not include the null byte
+  mv a0, t2
+  ecall 
+  
+  NEWLINE
+  
+  # Print RNA string to console
+  li a7, 4
+  la a0, msg_rna_converted
+  ecall
+  
+  li a7, 4
+  la a0, rna_string
+  ecall
+  
+  # -- EXIT -- 
+  # Call/exec macro to exit program
+  DONE
+  
---
+++ Discovery Series #10 RISC-V floating-point programming/sym2.asm
@@ -0,0 +1,82 @@
+# Matthew Jericho Go Sy (S11)
+
+.macro DONE
+	li a7, 10
+	ecall
+.end_macro
+
+.macro NEWLINE
+  li a7, 11
+  li a0, 10
+  ecall
+.end_macro
+
+.data
+	# Given
+	A: .float 2.0
+	B: .float -5.0
+	C: .float -3.0
+	
+	# Display
+	display1: .asciz "X1: "
+	display2: .asciz "X2: "
+
+.text
+init:
+	# ABCs
+	flw, f1, A, a1  # load address of A at a1 and its value in f1
+	flw, f2, B, a2  # load address of B at a2 and its value in f2
+	flw, f3, C, a3  # load address of C at a3 and its value in f3
+	
+	# constants
+	li t0, 2  # assign t4 to 2
+	fcvt.s.w f5, t0  # convert 2 at t4 to float in f4 
+	li t0, 4  # assign t5 to 4
+	fcvt.s.w f6, t0  # convert 4 at t5 to float in f5
+
+bottom: 
+	fmul.s f11, f5, f1  # 2a stored to f10
+
+top_right:
+	fmul.s f12, f2, f2  # b^2 stored to f11
+	fmul.s f13, f6, f1  # 4a stored to f12
+	fmul.s f13, f13, f3 # 4a (in f12) * c stored back to f12
+	fsub.s f14, f12, f13  # b^2 - 4ac stored to f13
+	fsqrt.s f15, f14  # sqrt(b^2 - 4ac) stored to f14
+	
+top_left:
+	fneg.s f16, f2  # -b
+	
+top_plus:
+	fadd.s f17, f16, f15  # -b + sqrt(b^2 - 4ac)
+	
+top_subtract:
+	fsub.s f18, f16, f15  # -b - sqrt(b^2 - 4ac)
+	
+whole1:
+	fdiv.s f19, f17, f11
+	
+whole2:
+	fdiv.s f20, f18, f11
+	
+answer:
+	li a7, 4
+	la a0, display1
+	ecall
+	
+	li a7, 2
+	fmv.s f10, f19
+	ecall
+	
+	NEWLINE
+	
+	li a7, 4
+	la a0, display2
+	ecall
+	
+	li a7, 2
+	fmv.s f10, f20
+	ecall
+	
+	DONE
+	
