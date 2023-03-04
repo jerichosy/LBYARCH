84857c409e4d40ccb85f570e4af73ff1c02d79e3
jerichosy <69558553+jerichosy@users.noreply.github.com>
2023-03-04 11:51:27 +0800

    Finished symMP1.asm
    Note: Comments may be incomplete and not reflect recent changes

--- RISC-V Machine Project/symMP1.asm
+++ RISC-V Machine Project/symMP1.asm
@@ -1,3 +1,5 @@
+# Note: Comments may be incomplete and not reflect recent changes
+
 .macro DONE
   li a7, 10
   ecall
@@ -39,9 +41,9 @@ check:
 determine_element_cnt_in_final:
 	# calculate number of elements in final matrix for use as loop counter
 	lw t0, Ar_size
-	lw t3, Bc_size
+	lw t2, Bc_size  # reassign t2 (to free up registers) bacause at this point t1=t2
 	
-	mul t4, t0, t3
+	mul t3, t0, t2
 	
 
 # SUBELEMENT
@@ -57,26 +59,82 @@ determine_element_cnt_in_final:
 # For A address counter, reset back to 
 # For B address counter, 
 
-subcompute_element:  # Loop this by no. of multiplications per element  ( no of multiplications = t1 (Ac_size) or t2 (Br_size) )
+determine_B_address_increment:
+	li a6, 4
+	mul a6, a6, t2  # based on no. of columns in B (Bc_size)
+	
+init_addresses:
+	la a3, C
+	la a4, A
+	la a5, B
+
+compute_element:  # Loop this by no. of multiplications per element  ( no of multiplications = t1 (Ac_size) or t2 (Br_size) )
+	# load A and B element
+	lw t4, 0(a4)
+	lw t5, 0(a5)
+	
+	# multiply
+	mul t5, t4, t5
+	
+	# sum
+	add t6, t6, t5
+
+	# increment A
+	addi a4, a4, 4
+	
+	# increment B
+	add a5, a5, a6
 	
 	# loop
 	addi t1, t1, -1  # decrement t1 (Ac_size)  [NOTE: In-place decrement)
-	bgtz t1, 
+	bgtz t1, compute_element 
 	
-init:
+compute_element_loop_conclusion:
+	# store word
+	sw t6, 0(a3)
+	addi a3, a3, 4
 	
+	# reset sum
+	li t6, 0
+	
+	# reset its loop counter
+	lw t1, Ac_size
 			
-compute_element:
+next_element:
+	# Check if there still more elements. If, not go to answer.
+	addi t3, t3, -1 
+	blez t3, answer_prefix
 
-	# store to C
+	# Prep next element loop
+	# if B is not done with B, reset B but add 4 * where what col we are in B
+	addi a2, a2, 1
+	blt a2, t2, next_element_B_not_done
 	
-	# prep next loop (reset address counters)
+	# else, increment A to next row
+	# reset a2
+	li a2, 0
 	
-	# loop upwards
-	addi t4, t4, 1  # decrement t4 (element count)
+	j next_element_B_done
 	
-	bgtz t4, sub_elements  # branch to loop if t4 (element count) still greater than zero
+next_element_B_not_done:
+	# reset A by substraction
+	li a1, 4
+	mul a1, a1, t1
+	sub a4, a4, a1
 	
+	# reset B, but add 4 * where what col we are in B
+	la a5, B
+	li a1, 4
+	mul a1, a1, a2
+	add a5, a5, a1
+	
+	j compute_element
+	
+next_element_B_done:
+	#addi a4, a4, 4
+	la a5, B	
+	
+	j compute_element	
 	
 answer_prefix:
 	# prefix the answer
@@ -85,12 +143,35 @@ answer_prefix:
 	ecall
 	
 	NEWLINE
-
-answer:	 # print answer by use of a loop depending on no. of rows (Ar_size), then append newline at end of loop
 	
+answer_init:
+	la a3, C
+	li a1, 0
+	
+answer_line:
+	# print
+	li a7, 1
+	lw a0, 0(a3)
+	# before ecall check if done printing
+	beqz a0, exit_program
+	ecall
+	
+	# print space
+	li a7, 11
+	li a0, 32
+	ecall
+	
+	# increment a3
+	addi a3, a3, 4
+	
+	# If C max col reached, print newline. Address will remain the same
+	addi a1, a1, 1
+	blt a1, t2, answer_line
+	
+	li a1, 0
 	NEWLINE
 	
-		
-	
-	# call exit
+	j answer_line
+
+exit_program:
 	DONE
